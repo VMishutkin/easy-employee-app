1f66ae2bbed8cb5d941e98604883fd4a8eddcbf3
VMishutkin <vladmish@list.ru>
2022-08-03 14:42:10 +0700

    Выполнил Повышенную сложность

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,108 @@ public class Main {
         employees[3] = new Employee("Иванов Петр Васильевич", 1, 67783);
         employees[4] = new Employee("Петров Василий Иванович", 2, 34578);
         employees[5] = new Employee("Иванов Василий Петрович", 2, 15633);
-        employees[6] = new Employee("Петров Петр Петрович", 5, 37733);
-        employees[7] = new Employee("Васильев Василий Иванович", 2, 24677);
+        employees[6] = new Employee("Петров Петр Петрович", 1, 37733);
+        employees[7] = new Employee("Васильев Василий Иванович", 3, 24677);
         employees[8] = new Employee("Иванов Иван Иванович", 4, 75899);
         employees[9] = new Employee("Капитан Джек Воробей", 1, 37348);
-
+        /*
         printAllEmployyes(employees);
         System.out.println("Суммарная зарплата сотрудников =  " + getSalarySummorize(employees));
         Employee loserEmployer = findMinimalSalary(employees);
         Employee luckyEmployer = findMaximalSalary(employees);
         System.out.println("Средняя зарплата сотрудников = " + getAverageSalary(employees));
         printEmployeesNames(employees);
+        //Повышенная сложность
+        indexingSalaries(employees, 10);
+
+        Employee[] anyDepartment = getDepartment(employees, 1);
+        printAllEmployyes(anyDepartment);
+        //printAllEmployyes(getDepartment(employees,2)); Можно вызывать так по отделу
+        Employee thisDepartmentLoser = findMinimalSalary(anyDepartment);
+        System.out.println("Суммарная зарплата в отделе " + getSalarySummorize(anyDepartment));
+        allActionsOnDepartment(employees,1, 5);
+         */
+
+        printMoreSalaries(employees, 30000);
+        printLessSalaries(employees, 50000);
+    }
+
+    private static Employee[] printLessSalaries(Employee[] employees, int upperThreshold) {
+        System.out.println("Сотрудники у которых зарплата меньше "+ upperThreshold + " рублей:");
+        int employeesMassiveSize=0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() < upperThreshold){
+                employeesMassiveSize++;
+            }
+        }
+        //System.out.println(" Размер отдела " + departmentSize + " Человек");
+        Employee[] emplyyesWithLessSalaries = new Employee[employeesMassiveSize];
+        int counter = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() < upperThreshold){
+                System.out.println(employees[i]);
+                emplyyesWithLessSalaries[counter++] = employees[i];
+                       }
+        }
+        return emplyyesWithLessSalaries;
+    }
+
+    private static Employee[] printMoreSalaries(Employee[] employees, int lowerThreshold) {
+        System.out.println("Сотрудники у которых зарплата больше "+ lowerThreshold + " рублей:");
+
+        int employeesMassiveSize=0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > lowerThreshold){
+                employeesMassiveSize++;
+            }
+        }
+        //System.out.println(" Размер отдела " + departmentSize + " Человек");
+        Employee[] emplyeesWithMoreSalaries = new Employee[employeesMassiveSize];
+        int counter = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getSalary() > lowerThreshold){
+                System.out.println(employees[i]);
+                emplyeesWithMoreSalaries[counter++] = employees[i];
+            }
+        }
+        return emplyeesWithMoreSalaries;
+
+    }
+
+    private static void allActionsOnDepartment(Employee[] employees, int departmentID,int salaryIndexing) {
+        Employee[] thisDepartment = getDepartment(employees,departmentID);
+        findMinimalSalary(thisDepartment);
+        findMaximalSalary(thisDepartment);
+        getSalarySummorize(thisDepartment);
+        indexingSalaries(thisDepartment,salaryIndexing);
+        printAllEmployyes(thisDepartment);
+
+    }
+
+    private static Employee[] getDepartment(Employee[] employees, int departmentID) {
+        int departmentSize=0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getDepartment() == departmentID){
+                departmentSize++;
+            }
+        }
+        //System.out.println(" Размер отдела " + departmentSize + " Человек");
+        Employee[] departmentEmployees = new Employee[departmentSize];
+        int departmentCounter = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i].getDepartment()==departmentID){
+                departmentEmployees[departmentCounter++] = employees[i];
+            }
+        }
+        return departmentEmployees;
+    }
+
+    private static void indexingSalaries(Employee[] employees, int procent) {
+        for (int i = 0; i < employees.length; i++) {
+            int newSalary = employees[i].getSalary() * (100 + procent) /100;
+            employees[i].setSalary(newSalary);
+        }
+
     }
 
     private static void printEmployeesNames(Employee[] employees) {
